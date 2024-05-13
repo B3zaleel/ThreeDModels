5dc57d8b8d82a0cf6d8b3876142c26bf91d5f42a
Bezaleel Olakunori <bezaleeloci@gmail.com>
2024-05-13 10:44:11 +0200

    Create a class that stores the known identifiers

---
+++ src/ThreeDModels/Format/Ctm/Identifier.cs
@@ -0,0 +1,13 @@
+namespace ThreeDModels.Format.Ctm;
+
+public static class Identifier
+{
+    public const int Magic = 0x4D54434F;
+    public const int Indices = 0x58444e49;
+    public const int Vertices = 0x54524556;
+    public const int Normals = 0x4d524f4e;
+    public const int UVMaps = 0x43584554;
+    public const int AttributeMaps = 0x52545441;
+    public const int MG2H = 0x4832474d;
+    public const int GridIndices = 0x58444947;
+}

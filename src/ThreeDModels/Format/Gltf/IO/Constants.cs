d81de4e9248d5fd8f88d7c690ffc91cda4fb5859
Bezaleel Olakunori <bezaleeloci@gmail.com>
2024-05-21 13:05:09 +0200

    Create a class that stores Gltf-related constant values

---
+++ src/ThreeDModels/Format/Gltf/IO/Constants.cs
@@ -0,0 +1,9 @@
+namespace ThreeDModels.Format.Gltf.IO;
+
+public static class Constants
+{
+    public const uint SupportedVersion = 2;
+    public const uint Magic = 0x46546C67;
+    public const uint Chunk_Type_JSON = 0x4E4F534A;
+    public const uint Chunk_Type_BIN = 0x004E4942;
+}

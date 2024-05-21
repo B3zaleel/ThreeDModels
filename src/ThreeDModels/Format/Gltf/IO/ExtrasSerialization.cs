107de9f832f84b835d150ba3b1af02c75b756e6a
Bezaleel Olakunori <bezaleeloci@gmail.com>
2024-05-21 10:35:59 +0200

    Create an unimplemented extras' serialization reader

---
+++ src/ThreeDModels/Format/Gltf/IO/ExtrasSerialization.cs
@@ -0,0 +1,10 @@
+namespace ThreeDModels.Format.Gltf.IO;
+
+public static class ExtrasSerialization
+{
+    public static object? Read(GltfReaderContext context)
+    {
+        // TODO: implement
+        throw new NotImplementedException();
+    }
+}

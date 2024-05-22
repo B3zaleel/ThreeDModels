b952aac42d81ac055791dad97ad7b51dd3527596
Bezaleel Olakunori <bezaleeloci@gmail.com>
2024-05-22 22:21:36 +0200

    Implement the KHR_draco_mesh_compression glTF extension

---
+++ src/ThreeDModels/Format/Gltf/Extensions/KHR_draco_mesh_compression.cs
@@ -0,0 +1,89 @@
+using System.Text.Json;
+using ThreeDModels.Format.Gltf.Elements;
+using ThreeDModels.Format.Gltf.IO;
+using static ThreeDModels.Format.Gltf.IO.Utf8JsonReaderHelpers;
+
+namespace ThreeDModels.Format.Gltf.Extensions;
+
+public class KHR_draco_mesh_compression : IGltfProperty
+{
+    /// <summary>
+    /// The index of the bufferView.
+    /// </summary>
+    public required int BufferView { get; set; }
+    /// <summary>
+    /// A dictionary object, where each key corresponds to an attribute and its unique attribute id stored in the compressed geometry.
+    /// </summary>
+    public required IntegerMap Attributes { get; set; }
+    public Dictionary<string, object?>? Extensions { get; set; }
+    public object? Extras { get; set; }
+}
+
+public class KhrDracoMeshCompressionExtension : IGltfExtension
+{
+    public string Name => nameof(KHR_draco_mesh_compression);
+    private const string _name_BufferView = "bufferView";
+    private const string _name_Attributes = "attributes";
+
+    public object? Read(ref Utf8JsonReader jsonReader, GltfReaderContext context, Type parentType)
+    {
+        if (parentType != typeof(MeshPrimitive))
+        {
+            throw new InvalidDataException("KHR_draco_mesh_compression must be used in a MeshPrimitive.");
+        }
+        int? bufferView = null;
+        IntegerMap? attributes = null;
+        Dictionary<string, object?>? extensions = null;
+        object? extras = null;
+        if (jsonReader.TokenType == JsonTokenType.PropertyName && jsonReader.Read())
+        {
+        }
+        if (jsonReader.TokenType == JsonTokenType.Null)
+        {
+            return null;
+        }
+        else if (jsonReader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new InvalidDataException("Failed to find start of property.");
+        }
+        while (jsonReader.Read())
+        {
+            if (jsonReader.TokenType == JsonTokenType.EndObject)
+            {
+                break;
+            }
+            var propertyName = jsonReader.GetString();
+            if (propertyName == _name_BufferView)
+            {
+                bufferView = ReadInteger(ref jsonReader);
+            }
+            else if (propertyName == _name_Attributes)
+            {
+                attributes = IntegerMapSerialization.Read(ref jsonReader, context);
+            }
+            else if (propertyName == PropertyNames.Extensions)
+            {
+                extensions = ExtensionsSerialization.Read<KHR_draco_mesh_compression>(ref jsonReader, context);
+            }
+            else if (propertyName == PropertyNames.Extras)
+            {
+                extras = ExtrasSerialization.Read(ref jsonReader, context);
+            }
+            else
+            {
+                throw new InvalidDataException($"Unknown property: {propertyName}");
+            }
+        }
+        if (bufferView == null || attributes == null)
+        {
+            throw new InvalidDataException("KHR_draco_mesh_compression.bufferView and KHR_draco_mesh_compression.attributes are required properties.");
+        }
+        return new KHR_draco_mesh_compression()
+        {
+            BufferView = (int)bufferView!,
+            Attributes = attributes!,
+            Extensions = extensions,
+            Extras = extras,
+        };
+    }
+}

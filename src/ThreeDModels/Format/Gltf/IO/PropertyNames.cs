86ea7a36b2ba24a44de1716fc4dc40d96ebfc322
Bezaleel Olakunori <bezaleeloci@gmail.com>
2024-05-22 22:20:56 +0200

    Create a class that stores pre-converted property names

---
+++ src/ThreeDModels/Format/Gltf/IO/PropertyNames.cs
@@ -0,0 +1,10 @@
+namespace ThreeDModels.Format.Gltf.IO;
+
+/// <summary>
+/// A collection of pre-converted property names.
+/// </summary>
+public static class PropertyNames
+{
+    public const string Extensions = "extensions";
+    public const string Extras = "extras";
+}

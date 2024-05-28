001a0f174294189b73f645b32b945bf505691ce3
Bezaleel Olakunori <bezaleeloci@gmail.com>
2024-05-28 10:57:36 +0200

    Implement MSFT_lod Gltf extension

---
+++ src/ThreeDModels/Format/Gltf/Extensions/MSFT_lod.cs
@@ -0,0 +1,76 @@
+using System.Text.Json;
+using ThreeDModels.Format.Gltf.Elements;
+using ThreeDModels.Format.Gltf.IO;
+using static ThreeDModels.Format.Gltf.IO.Utf8JsonReaderHelpers;
+
+namespace ThreeDModels.Format.Gltf.Extensions;
+
+/// <summary>
+/// Represents an object for specifying levels of detail (LOD).
+/// </summary>
+public class MSFT_lod : IGltfProperty
+{
+    /// <summary>
+    /// Array containing the indices of progressively lower LOD nodes.
+    /// </summary>
+    public List<int>? Ids { get; set; }
+    public Dictionary<string, object?>? Extensions { get; set; }
+    public object? Extras { get; set; }
+}
+
+public class MsftLodExtension : IGltfExtension
+{
+    public string Name => nameof(MSFT_lod);
+
+    public object? Read(ref Utf8JsonReader jsonReader, GltfReaderContext context, Type parentType)
+    {
+        if (parentType != typeof(Gltf))
+        {
+            throw new InvalidDataException("MSFT_packing_normalRoughnessMetallic must be used in a Gltf root.");
+        }
+        List<int>? ids = null;
+        Dictionary<string, object?>? extensions = null;
+        object? extras = null;
+        if (jsonReader.TokenType == JsonTokenType.PropertyName && jsonReader.Read())
+        {
+        }
+        if (jsonReader.TokenType == JsonTokenType.Null)
+        {
+            return null;
+        }
+        else if (jsonReader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new InvalidDataException("Failed to find start of property.");
+        }
+        while (jsonReader.Read())
+        {
+            if (jsonReader.TokenType == JsonTokenType.EndObject)
+            {
+                break;
+            }
+            var propertyName = jsonReader.GetString();
+            if (propertyName == nameof(ids))
+            {
+                ids = ReadIntegerList(ref jsonReader, context);
+            }
+            else if (propertyName == nameof(extensions))
+            {
+                extensions = ExtensionsSerialization.Read<MSFT_lod>(ref jsonReader, context);
+            }
+            else if (propertyName == nameof(extras))
+            {
+                extras = JsonSerialization.Read(ref jsonReader, context);
+            }
+            else
+            {
+                throw new InvalidDataException($"Unknown property: {propertyName}");
+            }
+        }
+        return new MSFT_lod()
+        {
+            Ids = ids,
+            Extensions = extensions,
+            Extras = extras,
+        };
+    }
+}
